440227b46e75033cc90f327159fbb1d511c4e846
Viktor Sadovnikov <viktor@jv-ration.com>
2016-11-27 23:51:10 +0100

    #1. Removing second plugin. One implementation handles both things: new task and registration of the model

--- project-info-gradle-plugin/src/main/java/com/jvr/gradle/plugins/info/ProjectInfoPlugin.java
+++ project-info-gradle-plugin/src/main/java/com/jvr/gradle/plugins/info/ProjectInfoPlugin.java
@@ -1,28 +1,29 @@
 package com.jvr.gradle.plugins.info;
 
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.invocation.Gradle;
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry;
 
 import javax.inject.Inject;
 
-public class ProjectInfoPlugin implements Plugin<Gradle> {
+public class ProjectInfoPlugin implements Plugin<Project> {
 
-    public static final String TASK_NAME = "project-info";
+    static final String TASK_NAME = "project-info";
 
-    public void apply(Gradle gradle) {
+    private final ToolingModelBuilderRegistry registry;
 
-        // At this moment projects' configurations are not loaded yet
-        // Therefore the task will be called later
-        gradle.allprojects(new Action<Project>() {
-            public void execute(Project project) {
-                if( project.getParent() == null ) {
-                    project.getTasks().create(TASK_NAME, ProjectInfoTask.class);
-                }
-            }
-        });
+    @Inject
+    public ProjectInfoPlugin(ToolingModelBuilderRegistry registry) {
+        this.registry = registry;
+    }
+
+    public void apply(Project project) {
+
+        registry.register(new ProjectInfoModelBuilder());
+
+        if (project.getParent() == null) {
+            project.getTasks().create(TASK_NAME, ProjectInfoTask.class);
+        }
 
     }
 
